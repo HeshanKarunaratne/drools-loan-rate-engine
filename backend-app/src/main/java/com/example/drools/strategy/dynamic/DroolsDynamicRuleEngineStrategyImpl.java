fb7aa144da8385666581ac55269bc1a1cdfaca26
HeshanKarunaratne <heshan947102@gmail.com>
2026-04-26 12:56:30 +0530

    chore: removed unwanted files and imports

--- backend-app/src/main/java/com/example/drools/strategy/dynamic/DroolsDynamicRuleEngineStrategyImpl.java
+++ backend-app/src/main/java/com/example/drools/strategy/dynamic/DroolsDynamicRuleEngineStrategyImpl.java
@@ -1,6 +1,5 @@
 package com.example.drools.strategy.dynamic;
 
-import com.example.drools.dto.DynamicRuleRequest;
 import com.example.drools.dto.RuleResult;
 import com.example.drools.dto.User;
 import lombok.extern.slf4j.Slf4j;
