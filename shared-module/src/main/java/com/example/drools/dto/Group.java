47845d10fc2513f2317cfc681543b0afc915076c
HeshanKarunaratne <heshan947102@gmail.com>
2026-06-22 16:39:43 +0530

    added Group model

---
+++ shared-module/src/main/java/com/example/drools/dto/Group.java
@@ -0,0 +1,12 @@
+package com.example.drools.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Group {
+    private int groupAgeCycles;
+}
